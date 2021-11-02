ed43b65f0a99ab9b73e060507ad0a99dd7056a1e
IgnatiusPang <i.pang@unsw.edu.au>
2021-11-03 04:04:03 +1100

    bug fix pca plot group

--- R/de_proteins_functions.R
+++ R/de_proteins_functions.R
@@ -270,7 +270,7 @@ rlePcaPlotList <- function(list_of_data_matrix, design_matrix,
                           ~plotPca(.x,
                                    design_matrix = design_matrix,
                                    sample_id_column = { { sample_id_column } },
-                                   group_column = { { group } },
+                                   group_column = { { group_column } },
                                    title = .y, cex = 7))
 
   list_of_plots <- c(rle_list, pca_list)
