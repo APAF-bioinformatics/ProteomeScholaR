b2147fca863d3b323633b2b4492399e0d8b19910
IgnatiusPang <i.pang@unsw.edu.au>
2021-12-24 03:28:05 +1100

    Update

--- Source/R/run_kinswingr.R
+++ Source/R/run_kinswingr.R
@@ -255,7 +255,7 @@ loginfo("Download information from UniProt.")
 uniprot_other_kinase_file<-file.path(args$tmp_dir,args$uniprot_other_kinase_file)
 if( ! file.exists( uniprot_other_kinase_file )) {
 
-  up <- UniProt.ws(taxId=args$taxonomy_id )
+  up <- UniProt.ws(taxId=as.integer(args$taxonomy_id ))
   list_of_sp_columns <- c("EXISTENCE"
                           , "SCORE"
                           , "REVIEWED"
