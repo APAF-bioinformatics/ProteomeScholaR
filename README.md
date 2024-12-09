258644b2edf1abfbeb912f6c3ce60d8d28dc74b5
Bucket Chemist <150747071+Bucket-Chemist@users.noreply.github.com>
2024-12-09 15:09:21 +1100

    added project_setup.R and updated readme

--- README.md
+++ README.md
@@ -4,31 +4,72 @@ editor_options:
     wrap: 72
 ---
 
-# ProteomeScholaR v0.8
+# ProteomeScholaR <img src="https://img.shields.io/badge/Version-0.8-green?style=for-the-badge" alt="Version 0.8">
+
+## Quick Start
+
+### 1. Required Software
+
+<a href="https://cran.r-project.org/bin/windows/base/" target="_blank">
+    <img src="https://img.shields.io/badge/Download-R_(Windows)-276DC3?style=for-the-badge&logo=r" alt="Download R for Windows">
+</a>
+<a href="https://cran.r-project.org/bin/macosx/" target="_blank">
+    <img src="https://img.shields.io/badge/Download-R_(macOS)-276DC3?style=for-the-badge&logo=r" alt="Download R for macOS">
+</a>
+<a href="https://posit.co/download/rstudio-desktop/" target="_blank">
+    <img src="https://img.shields.io/badge/Download-RStudio_Desktop-75AADB?style=for-the-badge&logo=rstudio" alt="Download RStudio">
+</a>
+
+### 2. Setup Script
+
+<a href="https://raw.githubusercontent.com/APAF-bioinformatics/ProteomeScholaR/main/project_setup.R" download="project_setup.R">
+    <img src="https://img.shields.io/badge/Download-Setup_Script-blue?style=for-the-badge&logo=r" alt="Download Setup Script">
+</a>
+
+## Setup Instructions
+
+1. Install RStudio Desktop if you haven't already (use button above)
+2. Download the setup script using the button above
+3. Open the downloaded file in RStudio
+4. Change the project name at the top of the script (optional: specify custom directory)
+5. Run the entire script (Ctrl+A then Ctrl+Enter)
+6. A new RStudio project will open automatically with all required files and structure
+
+## What Gets Set Up
+
+- Complete directory structure for proteomics analysis
+- Latest version of DIA workflow
+- Configuration files
+- R project file
+
+## Using the Workflow
+
+1. Run the DIA_workflow.rmd file (this should automatically have opened if you did the above correctly!)
+2. Please copy your organism .fasta to the data/UniProt subdirectory
+3. Please copy your searched data to the data/proteomics subdirectory
+4. Proceed chunk by chunk
+5. Use the embedded Shiny app to define your:
+   - Experimental design
+   - Contrasts
+   - Linear model
+6. Find all results in the summary_results folder
+7. Run DIA_report_1.0.rmd to generate a shareable report (current not finished)
 
 ## Contributors 
-* Ignatius Pang (ignatius.pang at mq.edu.au) 
-* Will Klare (william.klare at mq.edu.au) 
+* Ignatius Pang (ignatius.pang@mq.edu.au) 
+* Will Klare (william.klare@mq.edu.au) 
 
-## v0.8 provides a feature complete workflow for processing searched DIA-NN proteomics data.
-1. Download data and directory structure from [here](https://drive.google.com/file/d/1TO1a3muF0KKnRQN0W_qg1asCG1LgHZMB/view?usp=sharing).
+## Version Information
 
-1. Unzip.
+v0.8 provides a feature-complete workflow for processing searched DIA-NN proteomics data.
 
-1. Open R studio and set folder as Project.
+## Need Help?
 
-1. Copy DIA_workflow_1.0.rmd and DIA_report_1.0.rmd to the Project folder (scripts/proteomics folder) and open inside R studio.
+If you encounter any issues:
+1. Check the [Issues](https://github.com/APAF-bioinformatics/ProteomeScholaR/issues) page
+2. Contact the contributors
+3. Submit a new issue
 
-1. Run the DIA_workflow_1.0.rmd file, use the Shiny app to define your experimental design, contrasts and linear model you wish to use.
+## We very much encourage you to report any and all bugs using the issues page - we are commited to making this tool as robust as possible and we would love your help to do this :)
 
-1. Proceed chunk by chunk.
-
-1. Run DIA_report_1.0.rmd and generate a document that will dynamically produce a shareable report for your analysis.
-
-1. All the results are in the summary_results folder in the project root directory.
-
-1. Do the science :)
-
-Enjoy!
-
-
+Enjoy! 🧬🔬
---
+++ project_setup.R
@@ -0,0 +1,263 @@
+# ============================================================================
+# DIA Workflow Project Setup Script
+# ============================================================================
+# Hi there! Welcome to ProteomeScholaR :)
+# Instructions: 
+# 1. Set your project name and optional directory below
+# 2. Select ALL of this code (Ctrl+A or Cmd+A)
+# 3. Run it (Ctrl+Enter or Cmd+Enter)
+# ============================================================================
+
+# SET YOUR PROJECT OPTIONS HERE:
+my_project_name <- "my_analysis"  # Required: Name your project
+my_project_dir <- NULL            # Optional: Set specific directory (leave as NULL for default Documents folder)
+
+# Examples for my_project_dir:
+# Windows: my_project_dir <- "C:/Users/username/Projects"
+# Mac:     my_project_dir <- "~/Projects"
+
+# ============================================================================
+# DO NOT MODIFY CODE BELOW THIS LINE
+# ============================================================================
+
+# Install required packages if not already installed
+if (!requireNamespace("httr", quietly = TRUE)) {
+  install.packages("httr")
+}
+if (!requireNamespace("rstudioapi", quietly = TRUE)) {
+  install.packages("rstudioapi")
+}
+if (!requireNamespace("later", quietly = TRUE)) {
+  install.packages("later")
+}
+
+# Validate user input
+if (!is.character(my_project_name) || nchar(my_project_name) == 0) {
+  stop("Please set a valid project name at the top of the script")
+}
+
+# Define the setup function
+setup_dia_project <- function(root_dir = NULL, overwrite = FALSE) {
+  # Set default root_dir based on OS
+  if (is.null(root_dir)) {
+    if (.Platform$OS.type == "windows") {
+      docs_path <- file.path(Sys.getenv("USERPROFILE"), "Documents")
+    } else if (Sys.info()["sysname"] == "Darwin") {  # Mac OS
+      docs_path <- file.path(path.expand("~"), "Documents")
+    } else {  # Linux or other Unix-like systems
+      docs_path <- path.expand("~")
+    }
+    root_dir <- file.path(docs_path, "default_project")
+    
+    os_type <- if (.Platform$OS.type == "windows") {
+      "Windows"
+    } else if (Sys.info()["sysname"] == "Darwin") {
+      # Get Mac OS version
+      os_version <- system("sw_vers -productVersion", intern = TRUE)
+      mac_name <- if (grepl("^14", os_version)) {
+        "Sonoma (14)"
+      } else if (grepl("^13", os_version)) {
+        "Ventura (13)"
+      } else if (grepl("^12", os_version)) {
+        "Monterey (12)"
+      } else if (grepl("^11", os_version)) {
+        "Big Sur (11)"
+      } else if (grepl("^10.15", os_version)) {
+        "Catalina (10.15)"
+      } else if (grepl("^10.14", os_version)) {
+        "Mojave (10.14)"
+      } else if (grepl("^10.13", os_version)) {
+        "High Sierra (10.13)"
+      } else if (grepl("^10.12", os_version)) {
+        "Sierra (10.12)"
+      } else if (grepl("^10.11", os_version)) {
+        "El Capitan (10.11)"
+      } else if (grepl("^10.10", os_version)) {
+        "Yosemite (10.10)"
+      } else {
+        paste("MacOS", os_version)
+      }
+      message("MacOS version detected: ", mac_name)
+      "MacOS"
+    } else {
+      "Unix-like system"
+    }
+    message("Operating system detected: ", os_type)
+    message("Using default project location: ", root_dir)
+  }
+  
+  # Create directory structure
+  dirs <- list(
+    root = root_dir,
+    scripts = file.path(root_dir, "scripts"),
+    scripts_proteomics = file.path(root_dir, "scripts", "proteomics"),
+    data = file.path(root_dir, "data"),
+    data_proteomics = file.path(root_dir, "data", "proteomics"),
+    data_uniprot = file.path(root_dir, "data", "UniProt")
+  )
+  
+  # Create directories
+  for (dir in dirs) {
+    if (!dir.exists(dir)) {
+      dir.create(dir, recursive = TRUE)
+      message("Created directory: ", dir)
+    }
+  }
+  
+  # Define GitHub raw content URLs
+  templates <- list(
+    workflow = list(
+      url = "https://raw.githubusercontent.com/APAF-bioinformatics/ProteomeScholaR/dev-jr/Workbooks/DIA_workflow.rmd",
+      dest = file.path(root_dir, "DIA_workflow.rmd")
+    ),
+    config = list(
+      url = "https://raw.githubusercontent.com/APAF-bioinformatics/ProteomeScholaR/dev-jr/Workbooks/config.ini",
+      dest = file.path(dirs$scripts_proteomics, "config.ini")
+    )
+  )
+  
+  # Check if files exist
+  if (!overwrite && any(file.exists(sapply(templates, `[[`, "dest")))) {
+    stop("Destination files already exist. Use overwrite=TRUE to replace them.")
+  }
+  
+  # Download files
+  message("\nDownloading template files from GitHub...")
+  for (template in templates) {
+    response <- httr::GET(template$url)
+    if (httr::status_code(response) == 200) {
+      content <- httr::content(response, "raw")  # Get raw content instead of text
+      writeBin(content, template$dest)  # Write binary data directly
+      message("Successfully downloaded: ", basename(template$dest))
+    } else {
+      stop("Failed to download ", basename(template$dest), 
+           ". Status code: ", httr::status_code(response))
+    }
+  }
+  
+  message("\nProject setup complete!")
+  message("Project root: ", normalizePath(root_dir))
+  message("Workflow file: ", normalizePath(templates$workflow$dest))
+  message("Config file: ", normalizePath(templates$config$dest))
+  
+  return(invisible(list(
+    root_dir = normalizePath(root_dir),
+    workflow_file = normalizePath(templates$workflow$dest),
+    config_file = normalizePath(templates$config$dest),
+    directories = lapply(dirs, normalizePath)
+  )))
+}
+
+# Determine project path based on user input
+if (!is.null(my_project_dir)) {
+  project_path <- file.path(my_project_dir, my_project_name)
+} else {
+  if (.Platform$OS.type == "windows") {
+    project_path <- file.path(Sys.getenv("USERPROFILE"), "Documents", my_project_name)
+  } else {
+    project_path <- file.path(path.expand("~"), "Documents", my_project_name)
+  }
+}
+
+# Create and setup the project
+message("Creating project: ", project_path)
+setup_result <- setup_dia_project(project_path, overwrite = TRUE)
+
+# Create and open R project
+rproj_content <- c(
+  "Version: 1.0",
+  "",
+  "RestoreWorkspace: Default",
+  "SaveWorkspace: Default",
+  "AlwaysSaveHistory: Default",
+  "",
+  "EnableCodeIndexing: Yes",
+  "UseSpacesForTab: Yes",
+  "NumSpacesForTab: 2",
+  "Encoding: UTF-8",
+  "",
+  "RnwWeave: Sweave",
+  "LaTeX: pdfLaTeX"
+)
+rproj_file <- file.path(project_path, paste0(my_project_name, ".Rproj"))
+writeLines(rproj_content, rproj_file)
+message("Created R project file: ", rproj_file)
+
+# Create .Rprofile for automatic workflow opening
+startup_script <- file.path(project_path, ".Rprofile")
+startup_content <- sprintf('
+# Function to safely open workflow
+open_workflow <- function() {
+    message("Initializing project...")
+    Sys.sleep(5)  # Wait for project to fully load
+    
+    # Check if RStudio is actually available
+    if (!requireNamespace("rstudioapi", quietly = TRUE)) {
+        message("rstudioapi package not available")
+        return(FALSE)
+    }
+    
+    # Wait and retry logic for RStudio initialization
+    max_attempts <- 5
+    for(i in 1:max_attempts) {
+        if (rstudioapi::isAvailable()) {
+            break
+        }
+        message("Waiting for RStudio to initialize (attempt ", i, "/", max_attempts, ")...")
+        Sys.sleep(2)
+    }
+    
+    if (!rstudioapi::isAvailable()) {
+        message("RStudio not available after waiting. Please open the workflow file manually.")
+        return(FALSE)
+    }
+    
+    message("Opening workflow file...")
+    possible_files <- c(
+        file.path("%s", "DIA_workflow.rmd"),
+        file.path("%s", "DIA_workflow.Rmd"),
+        file.path("%s", "DIA_workflow.RMD")
+    )
+    
+    workflow_file <- possible_files[file.exists(possible_files)][1]
+    
+    if (!is.na(workflow_file)) {
+        tryCatch({
+            rstudioapi::navigateToFile(workflow_file)
+            message("Workflow file opened successfully")
+            return(TRUE)
+        }, error = function(e) {
+            message("Error opening workflow: ", e$message)
+            return(FALSE)
+        })
+    } else {
+        message("Warning: Could not find workflow file to open")
+        return(FALSE)
+    }
+}
+
+# Only run in interactive sessions
+if (interactive()) {
+    # Delay the execution slightly to ensure RStudio is ready
+    later::later(function() {
+        open_workflow()
+    }, 3)
+}
+', project_path, project_path, project_path)
+
+writeLines(startup_content, startup_script)
+message("Created startup script for automatic workflow opening")
+
+writeLines(startup_content, startup_script)
+message("Created startup script for automatic workflow opening")
+
+# Open the new project
+if (rstudioapi::isAvailable()) {
+  message("Opening new R project...")
+  message("Note: If you have unsaved changes, you'll be prompted to save them")
+  message("The workflow file will open automatically in the new project")
+  Sys.sleep(2)  # Give user time to read messages
+  rstudioapi::openProject(rproj_file)
+}
+
+message("\nSetup complete! Your new project is ready to use.")
