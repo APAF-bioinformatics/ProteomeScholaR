2563bc6d69ea39efc567894c8dace16a119f3047
Bucket-Chemist <william.klare@mq.edu.au>
2025-04-28 14:41:05 +1000

    made startup message dynamic to solve fork issue for multischolar

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,17 @@
 # R/zzz.R
 
 .onAttach <- function(libname, pkgname) {
+    # Construct the message dynamically using the pkgname variable
+    welcome_message <- paste0("Welcome to ", pkgname, "!\n\n")
+    load_deps_message <- paste0(
+        "IMPORTANT: Please run ", pkgname, "::loadDependencies() \n"
+        , "           to ensure all required packages are installed and loaded.\n"
+    )
+
     packageStartupMessage(
-        "-------------------------------------------------------------------\n",
-        "Welcome to ProteomeScholaR!\n\n",
-        "IMPORTANT: Please run ProteomeScholaR::loadDependencies() \n",
-        "           to ensure all required packages are installed and loaded.\n",
-        "-------------------------------------------------------------------"
+        "-------------------------------------------------------------------\n"
+        , welcome_message
+        , load_deps_message
+        , "-------------------------------------------------------------------"
     )
 }
